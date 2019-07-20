ecc827c68f2f2a258a3d752d5bb16baebeeb9456
Yuichi <47448414+824ty@users.noreply.github.com>
2019-07-20 20:32:55 +0900

    Add files via upload

--- test1.py
+++ test1.py
@@ -1,9 +1,22 @@
 #coding:utf-8
 
 import csv
+import datetime
+import random
 
-with open("test.csv","w+") as t:#test.tsvというファイルを新規作成。変数名「ｔ」
-    for s in range(1,500 + 1):
-        s = csv.writer (t, delimiter=",")
-        s.writerow(["one", "two", "three", "four", "five"])
-        s.writerow(["1", "2", "3", "4", "5"])
+datetime = random.uniform(datetime.datetime(2000, 1, 1,microsecond = 0), datetime.datetime(2019, 1, 1, microsecond = 0))
+
+
+with open("test.tsv","w+") as new_file:#test.tsvというファイルを新規作成。変数名「ｔ」
+    s = csv.writer (new_file, delimiter="\t")
+    fieldnames = ["log_id", "date", "latitude", "longitude"]
+    writer = csv.DictWriter(new_file, fieldnames=fieldnames , delimiter="\t")
+    writer.writeheader()
+
+    for a in range(1, 10000+1):
+        log = 1
+        lat = random.uniform(34.5, 35.5)
+        long = random.uniform(134, 139)
+        writer.writerow({"log_id": log, "date" : datetime, "latitude": lat, "longitude": long})
+
+        
